d32baad1969d4007fb31d1fff12d13d6b5c45c86
Hector <124hmunoz@frhsd.com>
2023-12-12 09:35:10 -0500

    all

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+//$("h3").css({border: "3px solid blue"});
+$(".wrapper").css({border: "3px solid red"});
+$("#clients").css({border: "3px solid yellow"});
+
+$("#contact-methods").css({border: "2px solid red"}).next().css({border: "2px solid green"}).closest("section").css({border;})
+var tweet ="<div style='margin: 20px 0; padding: 10px; background: #eee'>The big figth live: Ham vs Cheese!</div>"
+$("#tweets div").text(tweet);
+$("section").wrap("<div>");
+$("section").unwrap("");
+$("section").unwrap("<div>");
+
+$(".button").remove();
+$("#points-of-sale").empty();
+
+$("#contact img").removeAttr("alt");
+$("#contact img").attr("alt", "location");
+
+$("#social-nav").css("top", "-125px").css("left", "50px");
+
+});
+
